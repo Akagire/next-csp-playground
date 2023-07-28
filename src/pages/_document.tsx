c059a48d7c8d601b4c893e0ebd578b128304cd61
Takuya Eguchi <seal2501@gmail.com>
2023-07-28 11:22:24 +0900

    chore: install next-safe-middleware

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, type DocumentContext } from 'next/document'
+import {
+  getCspInitialProps, provideComponents,
+} from "@next-safe/middleware/dist/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await getCspInitialProps({ ctx });
+    return initialProps;
+  }
+
+  render() {
+    const { Head, NextScript } = provideComponents(this.props);
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
