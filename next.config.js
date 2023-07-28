8e3d0fb5c64459b2a03b4ba8948324d885082692
Takuya Eguchi <seal2501@gmail.com>
2023-07-28 11:46:29 +0900

    try sxx protection

--- next.config.js
+++ next.config.js
@@ -10,14 +10,14 @@ const nextConfig = {
             key: 'X-DNS-Prefetch-Control',
             value: 'on',
           },
-          // {
-          //   key: 'X-Frame-Options',
-          //   value: 'DENY',
-          // },
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
+            key: 'X-Frame-Options',
+            value: 'DENY',
           },
+          // {
+          //   key: 'X-XSS-Protection',
+          //   value: '1; mode=block',
+          // },
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
