10f54bbb9c588a132d8f888b86d402a5a4c37b17
Takuya Eguchi <seal2501@gmail.com>
2023-07-28 14:45:02 +0900

    try promise handling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,21 @@
-import Document, { Html, Main, Head, NextScript,
-  // type DocumentContext
+import Document, {
+  Html, Main,
+  // Head, NextScript,
+  type DocumentContext
 } from 'next/document'
-// import {
-//   getCspInitialProps, provideComponents,
-// } from "@next-safe/middleware/dist/document";
+import {
+  getCspInitialProps, provideComponents,
+} from "@next-safe/middleware/dist/document";
 
 export default class MyDocument extends Document {
-  // static async getInitialProps(ctx: DocumentContext) {
-  //   const initialProps = await getCspInitialProps({ ctx });
-  //   return initialProps;
-  // }
+  static getInitialProps(ctx: DocumentContext) {
+    // const initialProps = await getCspInitialProps({ ctx });
+    // return initialProps;
+    return getCspInitialProps({ ctx }).then((initialProps) => initialProps);
+  }
 
   render() {
-    // const { Head, NextScript } = provideComponents(this.props);
+    const { Head, NextScript } = provideComponents(this.props);
     return (
       <Html lang="ja">
         <Head />
