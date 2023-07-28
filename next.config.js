96eee097b0503822160055f35d3e7dc3edbf368f
Takuya Eguchi <seal2501@gmail.com>
2023-07-28 11:53:21 +0900

    try cache control

--- next.config.js
+++ next.config.js
@@ -26,22 +26,22 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'no-referrer',
           },
-          // {
-          //   key: 'Cross-Origin-Embedder-Policy',
-          //   value: 'unsafe-none',
-          // },
-          // {
-          //   key: 'Cross-Origin-Opener-Policy',
-          //   value: 'same-origin',
-          // },
-          // {
-          //   key: 'Cross-Origin-Resource-Policy',
-          //   value: 'cross-origin',
-          // },
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400', // 1 day
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
           },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin',
+          },
+          // {
+          //   key: 'Cache-Control',
+          //   value: 'public, max-age=86400', // 1 day
+          // },
           {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload', // 2 year
