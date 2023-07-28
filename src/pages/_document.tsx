5bb1e30ce0d514f82a5d227d1ab14a1fd1b00c91
Takuya Eguchi <seal2501@gmail.com>
2023-07-28 12:00:12 +0900

    try disable csp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,18 @@
-import Document, { Html, Main, type DocumentContext } from 'next/document'
-import {
-  getCspInitialProps, provideComponents,
-} from "@next-safe/middleware/dist/document";
+import Document, { Html, Main, Head, NextScript,
+  // type DocumentContext
+} from 'next/document'
+// import {
+//   getCspInitialProps, provideComponents,
+// } from "@next-safe/middleware/dist/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await getCspInitialProps({ ctx });
-    return initialProps;
-  }
+  // static async getInitialProps(ctx: DocumentContext) {
+  //   const initialProps = await getCspInitialProps({ ctx });
+  //   return initialProps;
+  // }
 
   render() {
-    const { Head, NextScript } = provideComponents(this.props);
+    // const { Head, NextScript } = provideComponents(this.props);
     return (
       <Html lang="ja">
         <Head />
