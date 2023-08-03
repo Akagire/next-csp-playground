0dbecb0598fe105323d06b3c9a7b22860172b9c9
Takuya Eguchi <seal2501@gmail.com>
2023-08-03 09:18:33 +0900

    chore: remove @next-safe/middleware

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,17 @@
-import Document, {
-  Html, Main,
-  // Head, NextScript,
-  type DocumentContext
-} from 'next/document'
-import {
-  getCspInitialProps, provideComponents,
-} from "@next-safe/middleware/dist/document";
+import Document, { DocumentContext, Html, Head, NextScript, Main } from "next/document";
 
 export default class MyDocument extends Document {
-  static getInitialProps(ctx: DocumentContext) {
-    // const initialProps = await getCspInitialProps({ ctx });
-    // return initialProps;
-    return getCspInitialProps({ ctx }).then((initialProps) => initialProps);
-  }
-
   render() {
-    const { Head, NextScript } = provideComponents(this.props);
     return (
-      <Html lang="ja">
-        <Head />
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
